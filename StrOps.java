71955a5e700b1bc01c9920683a53bbf1c3656045
OksBond <Oksi1979Q@gmail.com>
2016-10-17 15:40:56 +0300

    initial commit

--- StrOps.java
+++ StrOps.java
@@ -15,20 +15,20 @@ public class StrOps {
                 str1.length());
 
         //display str1 one char at a time - Отобразить строку strl посимвольно
-//        for (int i = 0; i < str1.length(); i++)
-//            System.out.print(str1.charAt(i));
-//        System.out.println();
-//        if (str1.equals(str2))
-//            System.out.println("str1 equal str2/strl эквивалентна str2");
-//        else
-//            System.out.println("str1 does not equals str2");
-//
-//        if (str1.equals(str3))
-//            System.out.println("str1 equal str3");
-//        else
-//            System.out.println("str1 does not equals str3");
-        result = str1.compareTo(str3); //Result==13 - Why???
-        //result=str1.compareTo(str3);
+        for (int i = 0; i < str1.length(); i++)
+            System.out.print(str1.charAt(i));
+        System.out.println();
+        if (str1.equals(str2))
+            System.out.println("str1 equal str2/strl эквивалентна str2");
+        else
+            System.out.println("str1 does not equals str2");
+
+        if (str1.equals(str3))
+            System.out.println("str1 equal str3");
+        else
+            System.out.println("str1 does not equals str3");
+
+        result = str1.compareTo(str3);//Result==13 - Why???
         if (result == 0)
             System.out.println("str1 and str3 are equal");
         else if (result < 0)
@@ -38,11 +38,9 @@ public class StrOps {
 
         //assing a new string to str2 - Присвоить переменной str2 новую строку
         str2 = "One Two Three One";
-
         idx = str2.indexOf("One");
         System.out.println("Index if first occurrence of One: " + idx);
-
-       idx = str2.lastIndexOf("One");
+        idx = str2.lastIndexOf("One");
         System.out.println("Index if last occurrence of One: " + idx);
     }
 }
